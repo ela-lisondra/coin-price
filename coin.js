52e242c893753b7d20b3bee5b36ce1cf52063756
ela_lisondra <emrlisondra@gmail.com>
2021-07-03 14:11:40 +0100

    use forEach() to style selected navLinks and to pull different currencies from the API, mobile and tablet responsive

--- coin.js
+++ coin.js
@@ -1,13 +1,29 @@
 const url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-const priceTag= document.querySelector("h1")
+const priceTag = document.querySelector("h1")
+const spanTag = document.querySelector("span")
 let currency = "USD"
 
 const checkPrice = function () {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            priceTag.innerHTML = data.bpi.USD.rate_float.toFixed(1)
+            priceTag.innerHTML = data.bpi[currency].rate_float.toFixed(1)
         })
 }
 
 checkPrice()
+
+const navLinks = document.querySelectorAll("nav a")
+navLinks.forEach(link => {
+    link.addEventListener("click", function (){
+        currency = this.getAttribute("data-currency")
+        checkPrice()
+
+        navLinks.forEach(link => link.classList.remove("selected"))
+
+        this.classList.add("selected")
+
+        spanTag.innerHTML = currency
+
+    })
+})
